b9293278804d77996704b174a01ab316292bff4c
ours12 <izumidraz@naver.com>
2022-05-12 18:37:02 +0900

    naverAPI 연결

--- src/main/java/com/example/myselectshop/controller/SearchRequestController.java
+++ src/main/java/com/example/myselectshop/controller/SearchRequestController.java
@@ -1,4 +1,24 @@
 package com.example.myselectshop.controller;
 
+
+import com.example.myselectshop.models.ItemDto;
+import com.example.myselectshop.utils.NaverShopSearch;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
 public class SearchRequestController {
+
+    private final NaverShopSearch naverShopSearch;
+
+    @GetMapping("/api/search")
+    public List<ItemDto> getItems(@RequestParam String query){
+        String result= naverShopSearch.search(query);
+        return naverShopSearch.fromJSONtoItems(result);
+    }
 }
